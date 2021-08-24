ac9f7b751e286401462574a94fbb3d33977fe46f
morningDude <89383213+morningDude@users.noreply.github.com>
2021-08-24 15:07:06 +0200

    console log

--- __tests__/generator.js
+++ __tests__/generator.js
@@ -78,10 +78,10 @@ describe('generate', () => {
 
   test('encode -> qr -> decode', async () => {
     const qr = await generate(payload, headers, signer)
-    // console.log('qr', qr)
+     console.log('qr', qr)
     const decoded = await verify(qr, verifier)
     expect(decoded).toEqual(payload)
-    // console.log('result', decoded)
+     console.log('result', decoded)
   })
 
 })
